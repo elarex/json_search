27b9bd6f1fc034bc9bb578a8932e9fc8014d884f
Jean le Roux <jeanlrx@gmail.com>
2023-07-30 13:17:37 +1000

    load up the json file provided or another one we ask for

--- main.rb
+++ main.rb
@@ -5,9 +5,28 @@ require "./search.rb"
 class Crm
 
   def self.run()
-    Client.load('clients.json')
+    # NOTE: default is to use the one we were given to save time, we can load another one
+    clients = Client.load('clients.json')
+
+    instructions()
+
+    while (cmd = readline().strip)
+      case cmd
+      when 'load'
+        filename = readline().strip
+        clients = Client.load(filename)
+      when 'search'
+      else
+        return
+      end
+    end
   end
 
+  private
+
+  def self.instructions
+    p "Commands are load, search. Anything else quits."
+  end
 
 end
 
