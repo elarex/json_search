c505dd6d5c659ee309c477b825ac0b1eeb6f51c2
Jean le Roux <jeanlrx@gmail.com>
2023-07-30 13:50:40 +1000

    update saerch spec to add missing function test

--- search_spec.rb
+++ search_spec.rb
@@ -26,14 +26,17 @@ describe Search do
       expect(search.find(key: 'to_s', query: 'test')).to eq([])
     end
   end
+
+  #PRIVATE BLOW THIS
+
   describe ".find_by" do
     it "compares partial strings" do
       entry = Object.new
-      allow(entry).to receive(:full_name).and_return("something")
+      allow(entry).to receive(:name).and_return("something")
       another = Object.new
-      allow(another).to receive(:full_name).and_return("something else")
+      allow(another).to receive(:name).and_return("something else")
       search = Search.new(list: [entry, another])
-      expect(search.send(:find_by, key: :full_name, query: "thing")).to eq([entry, another])
+      expect(search.send(:find_by, key: :name, query: "thing")).to eq([entry, another])
     end
 
     it "compares exact integers" do
@@ -43,6 +46,15 @@ describe Search do
       allow(another).to receive(:number).and_return(1234)
       search = Search.new(list: [entry, another])
       expect(search.send(:find_by, key: :number, query: 123)).to eq([entry])
+    end
+  end
+
+  describe ".valid_key?" do
+    it "returns true if the list entries respond to a key" do
+      entry = Object.new
+      search = Search.new(list: [entry])
+      expect(search.send(:valid_key?, 'nonsense')).to eq(false)
+      expect(search.send(:valid_key?, 'to_s')).to eq(true)
     end
   end
 end
